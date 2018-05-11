95fa939f3c67a4682da6f69c6a9a33f8121d8356
num81 <jasonharp@internode.on.net>
2018-05-11 18:10:19 +1000

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -50,7 +50,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
 
 static CBlock CreateGenesisBlock(uint32_t nTime, const uint256& nNonce, const std::vector<unsigned char>& nSolution, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "Snowgem19ac02b7cdc7d9e50c765bbc6146c3dd3adb8e93cf7cbe9bbc7ec290f8950182";
+    const char* pszTimestamp = "Snowgema9f525a520bea48ab5628cec79ee0af33cf234bea3ee106db4a76233243896e0";
     const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nSolution, nBits, nVersion, genesisReward);
 }
